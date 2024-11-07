2d91b920b2aa3bab345e77db3a276ed83bbb7d0b
Zoeyzyy <1162651439@qq.com>
2024-11-07 14:55:09 +0800

    Fix: add all command

--- src/main/java/command/concreteCommand/IOCommand/SaveCommand.java
+++ src/main/java/command/concreteCommand/IOCommand/SaveCommand.java
@@ -18,12 +18,6 @@ public class SaveCommand implements Command {
         document.save(filePath);
     }
 
-/*************  ✨ Codeium Command ⭐  *************/
-/**
- * Undo operation for SaveCommand.
- * This command cannot be undone or redone, hence this method provides no functionality.
- */
-/******  c9659552-71c8-4471-a626-a648bb96d0de  *******/
     @Override
     public void undo() {
         // save 不能undo 和 redo
