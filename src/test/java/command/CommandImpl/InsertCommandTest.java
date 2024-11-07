c5fc12c2f17706081c5917ca1dbfa8e3bacaef1b
Zoeyzyy <1162651439@qq.com>
2024-11-07 21:29:14 +0800

    Feature: add insert test

---
+++ src/test/java/command/CommandImpl/InsertCommandTest.java
@@ -0,0 +1,23 @@
+package test.java.command.CommandImpl;
+
+import command.CanUndoCommand;
+import command.Command;
+import command.commandImpl.editCommand.InsertCommand;
+import document.HTMLDocument;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test;
+
+public class InsertCommandTest {
+
+    @Test
+    public void executeAndUndo() {
+        HTMLDocument doc = new HTMLDocument(null);
+        InsertCommand insertCommand = new InsertCommand(doc, "div", "id1", "body", "hello world");
+        insertCommand.execute();
+        assertEquals("Hello World", doc.findElementById("id1").getTextContent());
+
+        insertCommand.undo();
+        assertEquals(null, doc.findElementById("id1").getTextContent());
+    }
+}
