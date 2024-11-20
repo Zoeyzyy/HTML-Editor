8f9b4959703f3674db02e67be2bd43dc21b95923
Zoeyzyy <1162651439@qq.com>
2024-11-20 19:00:53 +0800

    Fix: fix file read in command test

--- src/test/java/command/CommandImpl/IOCommand/SaveCommandTest.java
+++ src/test/java/command/CommandImpl/IOCommand/SaveCommandTest.java
@@ -3,8 +3,10 @@ package command.CommandImpl.IOCommand;
 import command.commandImpl.IOCommand.SaveCommand;
 import document.HTMLDocument;
 import org.junit.jupiter.api.Test;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,18 +19,23 @@ public class SaveCommandTest {
         SaveCommand saveCommand = new SaveCommand(document, "test.html");
         saveCommand.execute();
 
-        try {
-            Path filePath = Path.of("test.html");
-            String content = Files.readString(filePath);
-            assertEquals("<html>\n" +
-                    " <head>\n" +
-                    "  <title></title>\n" +
-                    " </head>\n" +
-                    " <body>\n" +
-                    " </body>\n" +
-                    "</html>", content);
-        } catch (Exception e) {
+        String filePath = "test.txt";
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        assertEquals("<html>\n" +
+                " <head>\n" +
+                "  <title></title>\n" +
+                " </head>\n" +
+                " <body>\n" +
+                " </body>\n" +
+                "</html>", content.toString());
     }
 }
