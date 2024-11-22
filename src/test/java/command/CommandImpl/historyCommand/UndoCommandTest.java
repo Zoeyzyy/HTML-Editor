e2c9de8aaff099b092fe8317cd88693adb1f1aea
Zoeyzyy <1162651439@qq.com>
2024-11-22 17:03:47 +0800

    Fix: add storeCommand in hostryCMDtest

--- src/test/java/command/CommandImpl/historyCommand/UndoCommandTest.java
+++ src/test/java/command/CommandImpl/historyCommand/UndoCommandTest.java
@@ -18,27 +18,33 @@ public class UndoCommandTest {
         // Test all undoable commands
         AppendCommand appendCommand = new AppendCommand(editor, "div", "id1", "body", "Hello World");
         appendCommand.execute();
+        editor.storeCommand(appendCommand);
         assertEquals("Hello World", editor.getDocument().findElementById("id1").getTextContent());
 
         InsertCommand insertCommand = new InsertCommand(editor, "div", "id2", "body", "Hello HTML");
         insertCommand.execute();
+        editor.storeCommand(insertCommand);
         assertEquals("Hello HTML", editor.getDocument().findElementById("id2").getTextContent());
 
         EditIDCommand editIDCommand = new EditIDCommand(editor, "id1", "id3");
         editIDCommand.execute();
+        editor.storeCommand(editIDCommand);
         assertNull(editor.getDocument().findElementById("id1"));
         assertEquals("id3", editor.getDocument().findElementById("id3").getId());
 
         EditTextCommand editTextCommand = new EditTextCommand(editor, "id3", "Hello HTML");
         editTextCommand.execute();
+        editor.storeCommand(editTextCommand);
         assertEquals("Hello HTML", editor.getDocument().findElementById("id4").getTextContent());
 
         // Test not undoable commands
         PrintTreeCommand printTreeCommand = new PrintTreeCommand(editor);
         printTreeCommand.execute();
+        editor.storeCommand(printTreeCommand);
 
         DeleteCommand deleteCommand = new DeleteCommand(editor, "id4");
         deleteCommand.execute();
+        editor.storeCommand(deleteCommand);
         assertNull(editor.getDocument().findElementById("id4"));
 
         UndoCommand undoCommand = new UndoCommand(editor);
