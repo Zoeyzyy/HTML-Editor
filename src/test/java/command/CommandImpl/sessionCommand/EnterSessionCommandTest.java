a18c8922116f694d4513bc71c8f0b92116c235f7
Zoeyzyy <1162651439@qq.com>
2024-11-23 14:22:46 +0800

    Fix: entersessionCmdTest: test reenter session status

--- src/test/java/command/CommandImpl/sessionCommand/EnterSessionCommandTest.java
+++ src/test/java/command/CommandImpl/sessionCommand/EnterSessionCommandTest.java
@@ -1,7 +1,9 @@
 package command.CommandImpl.sessionCommand;
 
+import command.commandImpl.displayCommand.ShowIDCommand;
 import command.commandImpl.sessionCommand.EnterSessionCommand;
 import command.commandImpl.sessionCommand.ExitSessionCommand;
+import command.commandImpl.sessionCommand.LoadCommand;
 import org.junit.jupiter.api.Test;
 import session.Session;
 
@@ -11,14 +13,25 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class EnterSessionCommandTest {
     @Test
     public void execute() {
-        Session session = new Session("example.html");
+        Session session = new Session("default");
+        String currentPath = System.getProperty("user.dir");
+        String fileName = "//src//main//resources//template.html";
+        LoadCommand loadCommand = new LoadCommand(session, fileName);
+        loadCommand.execute();
+
+        ShowIDCommand showIDCommand = new ShowIDCommand(session.getActiveEditor(), true);
+        showIDCommand.execute();
+
         ExitSessionCommand exitSessionCommand = new ExitSessionCommand(session);
         exitSessionCommand.execute();
-        assertNull(session.getActiveEditor());
 
         EnterSessionCommand enterSessionCommand = new EnterSessionCommand(session);
         enterSessionCommand.execute();
-        // TODO: how to check the session
-        assertEquals("example.html", session.getActiveEditor().getFileName());
+        // check: editlist, active editor, file showid, no redo/undo history
+        assertEquals(">" + currentPath + "src\\main\\resources\\template.html", session.getEditorList().get(0));
+        assertEquals(1, session.getEditorList().size());
+        assertEquals(true, session.getActiveEditor().getDocument().getShowID());
+//        assertEquals(0, session.getActiveEditor().getRedohistoy().size());
+//        assertEquals(0, session.getActiveEditor().getUndohistory().size());
     }
 }
